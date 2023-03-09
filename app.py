20f23f779eb649092f6c4e9d076925e129e2cb27
Alyssa Denary <alyssa.denary@gmail.com>
2023-03-09 15:22:20 -0500

    add pseudocode for routes

--- app.py
+++ app.py
@@ -7,3 +7,25 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+# root route
+# renders title, instructions, button start survey
+#   Button action will eventually take user to /questions/0 (rn form action
+#   takes user to /begin)
+
+# POST reroute survey_start form from /begin to /questions/0
+#   /questions/<q_number>
+#   either hardcode on length of responses, or future cookies?
+# GET request for updated /questions/0 page
+#   render the question.html jinja template
+
+# POST route for /answer
+#   record answer in responses
+#   take user to next question by input of question
+#   return parameter of q_number of next question
+#   return redirect("/questions/<q_number>")
+
+
+# request.form["comment"]
