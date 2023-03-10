449624a51b6ddb0d65c3e490c501fea60c4e5136
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-03-09 16:57:26 -0800

    implemented feedback from code review

--- app.py
+++ app.py
@@ -10,10 +10,6 @@ debug = DebugToolbarExtension(app)
 
 responses = []
 
-# root route
-# renders title, instructions, button start survey
-#   Button action will eventually take user to /questions/0 (rn form action
-#   takes user to /begin)
 @app.get("/")
 def root_route():
     """ Render the start page for the survey, passing in
@@ -25,24 +21,17 @@ def root_route():
         title=survey.title,
         instructions=survey.instructions)
 
-# POST reroute survey_start form from /begin to /questions/0
-#   /questions/<q_number>
-#   either hardcode on length of responses, or future cookies?
 @app.post("/begin")
 def begin_survey_route():
-    """ POST route for starting survey or redirecting
-        user to last completed question in survey.
+    """ POST route for starting survey.
 
-        Send the user to the question that corresponds with
-        the length of the responses list.
+        Clears any existing responses and starts survey at question 0.
     """
 
+    responses.clear()
+    return redirect("/questions/0")
 
-    q_number = len(responses)
-    return redirect(f"/questions/{q_number}")
 
-# GET request for updated /questions/0 page
-#   render the question.html jinja template
 @app.get("/questions/<int:q_number>")
 def survey_question(q_number):
     """ Render a single survey question on a page.
@@ -50,36 +39,25 @@ def survey_question(q_number):
 
     return render_template(
         "question.html",
-        question=survey.questions[q_number],
-        q_number=q_number
+        question=survey.questions[q_number]
     )
 
-# POST route for /answer
-#   record answer in responses
-#   take user to next question by input of question
-#   return parameter of q_number of next question
-#   return redirect("/questions/<q_number>")
 @app.post("/answer")
 def answer_route():
     """ POST route that:
         - Records the answer given to an individual question
-        - Increments the question number by 1
         - Checks if question number < num of survey questions
             - If yes, redirect to next survey question
             - If no, redirect to thank you page
     """
 
-    global responses
     responses.append(request.form["answer"])
-
-    q_number = int(request.form["index"])
-    q_number += 1
+    q_number = len(responses)
 
     if q_number < len(survey.questions):
-        # route to next quesiton
         return redirect(f"/questions/{q_number}")
+
     else:
-        # redirect to thank you page
         return redirect("/completion")
 
 @app.get("/completion")
